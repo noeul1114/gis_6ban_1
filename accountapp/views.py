a5068fa9ed503a1d479af937183a8201540496f3
noeul1114 <parkhyongsok@naver.com>
2021-07-12 12:05:47 +0900

    7/12 6ban class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -14,9 +14,12 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'new_hello_world': new_hello_world})
+                      context={'hello_world_list': hello_world_list})
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'hello_world_list': hello_world_list})
 
