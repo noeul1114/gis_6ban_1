4989f6b57db40cc8b01b601b86da465015db140f
noeul1114 <parkhyongsok@naver.com>
2021-07-12 11:27:12 +0900

    7/12 6ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
@@ -9,8 +10,12 @@ def hello_world(request):
 
         temp = request.POST.get('input_text')
 
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD!'})
